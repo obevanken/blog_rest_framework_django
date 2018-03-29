d22203660d9628db01b70b874aa3605482d80299
obevanken <vanen11@bk.ru>
2018-03-29 17:22:45 +0700

    requer

--- articles/tests.py
+++ articles/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
+from .models import Articles
 
-# Create your tests here.
+class ArticlesTestCase(TestCase):
+    def create(self):
+        Articles.objects.create(title = "Test", body = "TestBody")
